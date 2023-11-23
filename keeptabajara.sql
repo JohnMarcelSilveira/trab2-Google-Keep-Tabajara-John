7226c590dfeb0a0e33b0bb031589498e4eb1b1f5
John Silveira <john.silveira.pb@compasso.com.br>
2023-11-23 20:08:33 -0300

    ♻️Refac: Refactor anotacao table schema

--- keeptabajara.sql
+++ keeptabajara.sql
@@ -4,28 +4,14 @@ CREATE DATABASE keep_tabajara;
 
 \c keep_tabajara;
 
-CREATE TABLE cor(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    nome VARCHAR(20) NOT NULL
-);
-
 CREATE TABLE anotacao(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     titulo VARCHAR(50) NOT NULL,
-    descricao VARCHAR(200) NOT NULL,
+    descricao TEXT NOT NULL,
     data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     data_edicao TIMESTAMP,
+    cor VARCHAR(50) DEFAULT 'FFFF00',
     foto BYTEA,
     lixeira BOOLEAN DEFAULT FALSE,
-    data_exclusao TIMESTAMP,
-    cor_id UUID REFERENCES cor(id)    
+    data_exclusao TIMESTAMP    
 );
-
--- 1
-INSERT INTO cor (nome) VALUES 
-('Vermelho'),
-('Azul'),
-('Verde'),
-('Amarelo'),
-('Preto'),
-('Branco');
